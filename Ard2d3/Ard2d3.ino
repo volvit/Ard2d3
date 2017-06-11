2a478d79cd148165a03cc127a6256b009b67e1a9
VolVit 76 94 volodya <VolVit 76 94 volodya>
2017-06-11 09:36:10 +0300

    Добавьте файлы проекта.

---
+++ Ard2d3/Ard2d3.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Ard2d3.ino
+ Created:	11.06.2017 9:36:08
+ Author:	volv
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
